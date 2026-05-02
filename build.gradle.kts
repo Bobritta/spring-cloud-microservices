013751ff3fd59d60e9391b1e2185def1e97c1bf0
Tanya <hummingbird_t@mail.ru>
2026-05-02 15:37:15 +0300

    init: config server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot") version "3.2.2" apply false
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.3.4" apply false
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 allprojects {
@@ -23,7 +23,7 @@ subprojects {
 
     dependencyManagement {
         imports {
-            mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.1")
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
         }
     }
 }
